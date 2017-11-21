a8b3fb05b9da6caa5519f06fa5b2f43ad6462827
Glitch (warp-space) <none>
2017-11-21 15:15:09 +0000

    🎪👧 Updated with Glitch

--- server.js
+++ server.js
@@ -3,15 +3,26 @@
 // EXAMPLE 2: Send a JSON object back all API like
 // ===============================================
 
+// import express
 const express = require('express');
+// init a new express app
 const app = express();
 
+// typically, this kind of data will live in a database layer, separate
+// from the server.js file. You'll learn about the database layer later
+// on, so for now we've included the data in-memory.
+const accountData = {
+  accountID: '15486906',
+  firstName: 'Sally',
+  lastName: 'Student',
+  accountStatus: 'active'
+};
 
 // `res.json` converts JavaScript objects to JSON and
 // appropriately sets the Content-Type header to 
 // application/json; charset=utf-8 . By default,
 // we'll get a 200 HTTP status code.
-app.get('/', (req, res) => res.json({foo: 'bar'}));
+app.get('/my-account', (req, res) => res.json(accountData));
 
 // listen for requests
 app.listen(process.env.PORT || 8080, () => console.log(
